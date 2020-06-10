c8a2972b66f7137be1584ea4a9f6f0e466969f3d
David <didicp4@gmail.com>
2020-06-10 16:41:52 +0100

    feat: change react logo to eddie bot and correct title
    The image on the logo may change in the future.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Image } from 'semantic-ui-react';
 
 import "./header.styles.scss";
 
@@ -7,6 +8,12 @@ const Header = () => (
   <header className="header">
     <nav>
       <ul>
+        <li className="logo">
+          <Link to="/">
+            <Image src="/logo512.png" size="mini" />
+            <span className="logo-text">EddieBot</span>
+          </Link>
+        </li>
         <li>
           <Link to="/">Community Statistics</Link>
         </li>
