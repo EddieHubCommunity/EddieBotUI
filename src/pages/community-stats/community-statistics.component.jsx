d5fc3fedd1196fee935d08210ab2d23f45365444
Remco Halman <remco.halman@gmail.com>
2020-09-19 22:33:25 +0200

    Update community-statistics.component.jsx

--- src/pages/community-stats/community-statistics.component.jsx
+++ src/pages/community-stats/community-statistics.component.jsx
@@ -4,28 +4,28 @@ import Statistics from "./statistics.component";
 import "./community-statistics.styles.scss";
 
 const CommunityStatisticsPage = () => {
-    return (
-        <div className="community-statistics-page">
-            <h1 className="title">Community Statistics</h1>
-            <div className="statistics-list">
-                <Statistics
-                    className="messages"
-                    title="Total Messages"
-                    value={1000}
-                />
-                <Statistics
-                    className="users"
-                    title="Total Users"
-                    value={1230}
-                />
-                <Statistics
-                    className="reactions"
-                    title="Reactions"
-                    value={35000}
-                />
-            </div>
+  return (
+    <div className="community-statistics-page">
+      <h1 className="title">Community Statistics</h1>
+        <div className="statistics-list">
+          <Statistics
+            className="messages"
+            title="Total Messages"
+            value={1000}
+          />
+          <Statistics
+              className="users"
+              title="Total Users"
+              value={1230}
+          />
+          <Statistics
+              className="reactions"
+              title="Reactions"
+              value={35000}
+          />
         </div>
-    );
+    </div>
+  );
 };
 
 export default CommunityStatisticsPage;
