825706e243dcdfc33e759fb36dd714d6b105c614
Remco Halman <remco.halman@gmail.com>
2020-09-19 22:42:33 +0200

    Update statistics.component.jsx

--- src/pages/community-stats/statistics.component.jsx
+++ src/pages/community-stats/statistics.component.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import "./statistics.styles.scss";
 
 /**
- * @param {object} props
  * @param {string} [props.className]
  * @param {string} props.title
  * @param {string} props.icon - Check this link to know the possible values: https://react.semantic-ui.com/elements/icon/
  * @param {any} props.value
  */
 const Statistics = ({ className = "", title, icon, value }) => (
-    <div className={`statistic ${className}`}>
-        <span className="title">{title}</span>
-        <span className="value">{value}</span>
-    </div>
+  <div className={`statistic ${className}`}>
+    <span className="title">{title}</span>
+    <span className="value">{value}</span>
+  </div>
 );
 
 export default Statistics;
