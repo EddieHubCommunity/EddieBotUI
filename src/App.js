62c1b317243793fb174f7371612a7cf1d68a1a0b
Rossano D'Angelo <rossanodan@gmail.com>
2020-09-21 17:15:10 +0100

    rossanodan-issue-35: started working on the home page component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
+/* eslint-disable no-undef */
+
 import React from "react";
-import { Link } from "react-router-dom";
-import { Navbar, Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
-import logo from './assets/eddie-jaoude.png';
-
-import Routes from "./app-routes/app-routes";
+import ComingSoon from './coming-soon/coming-soon';
+import HomePage from './home-page/home-page';
 
 const App = () => (
-  <>
-    <Navbar bg="light">
-      <Navbar.Brand as={Link} to="/">
-        <img
-          src={logo}
-          width="20"
-          className="d-inline-block align-top"
-          alt="EddieBotUI"
-        />
-      </Navbar.Brand>
-    </Navbar>
-    <Container className='mt-4'>
-      <Row>
-        <Col>
-          <Routes />
-        </Col>
-      </Row>
-    </Container>
-  </>
+  <Container>
+    <Row>
+      <Col>
+        { process.env.REACT_APP_COMING_SOON === "false" ? <ComingSoon /> : <HomePage /> }
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default App;
