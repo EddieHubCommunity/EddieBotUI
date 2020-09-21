857708c7407bb5dd7153d9cf0c26394a703e065a
Rossano D'Angelo <rossanodan@gmail.com>
2020-09-21 15:02:57 +0100

    feat(holding): coming soon page #33
    * rossanodan-issue-33
    - Added not found component
    - Added coming soon component and placeholder test
    - Added application router and placeholder test
    - Added env file and enabled coming soon mode
    
    * rossanodan-issue-33
    - Added PropTypes validation
    - Added onClick event to go to homepage button in NotFound
    
    * rossanodan-issue-33
    - Added blank line at the bottom of files

---
+++ src/feature-comingsoon/comingsoon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ComingSoon = () => (
+  <div className="h-screen w-screen bg-gray-100 flex items-center">
+    <div className="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
+        <div className="max-w-md">
+          <div className="text-5xl font-dark font-bold">Coming soon</div>
+            <br />
+            <p className="mb-8">But dont worry, awesome things are on the way.</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default ComingSoon;
