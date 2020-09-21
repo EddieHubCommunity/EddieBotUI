9521a37ec65dc1f1f3f828369b2ae61560e07145
Rossano D'Angelo <rossanodan@gmail.com>
2020-09-21 17:25:16 +0100

    rossanodan-issue-35: fixed iframe error adding title property

--- src/coming-soon/coming-soon.jsx
+++ src/coming-soon/coming-soon.jsx
@@ -4,7 +4,7 @@ const ComingSoon = () => (
   <>
     <h1>Coming soon</h1>
     <p>Don&apos;t worry, amazing things are on the way. Stay tuned!</p>
-    <iframe src="https://giphy.com/embed/3o72FkiKGMGauydfyg" width="480" height="363" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/arielle-m-coming-soon-3o72FkiKGMGauydfyg">via GIPHY</a></p>
+    <iframe src="https://giphy.com/embed/3o72FkiKGMGauydfyg" title="coming-soon" width="480" height="363" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/arielle-m-coming-soon-3o72FkiKGMGauydfyg">via GIPHY</a></p>
   </>
 );
 
